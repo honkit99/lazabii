af43ad9f32c4b782526ceb7672dee25fb7e5232a
honkit99 <81602822+honkit99@users.noreply.github.com>
2021-03-31 13:46:48 +0800

    userAdminDB

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,10 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->integer('gender');
+            $table->string('phone');
+            $table->dateTime('dob');
+            $table->integer('country_id');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
