6713be78533136bda5e725bda84953a163377505
honkit99 <81602822+honkit99@users.noreply.github.com>
2021-03-31 17:39:04 +0800

    ee

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,14 +22,14 @@
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            @if (Route::has('login'))
+            @if (Route::has('user.login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
                         <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
                     @else
                         <a href="{{ route('admin.login') }}" class="text-sm text-gray-700 underline">Log in</a>
 
-                        @if (Route::has('register'))
+                        @if (Route::has('user.register'))
                             <a href="{{ route('admin.register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
                         @endif
                     @endauth
